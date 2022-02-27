f134cbbb20bdc1969ad8b8d91ed8facefcbc0d50
Amoghwhitehat <92363610+Amoghwhitehat@users.noreply.github.com>
2022-02-27 14:28:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup()
+{
+    canvas=createCanvas(450,450);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw()
+{
+    image(video,0,0,450,450);
+}
